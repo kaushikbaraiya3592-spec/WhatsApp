89239d0f3cfd74a5e84dbd033d44a4f6f03c14a5
Kaushik baraiya <kaushikbaraiya3592@gmail.com>
2026-01-15 16:31:22 +0530

    Update fullScreenPage.dart

--- lib/fullScreenPage.dart
+++ lib/fullScreenPage.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 class FullScreenPage extends StatelessWidget {
   final List wallpapers;
   final int index;
